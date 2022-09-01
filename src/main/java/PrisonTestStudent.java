096087e2944832b1657226feff9c17e41829c4c3
JaidenAM <Jaiden.Miller@cdpipelinedevshop.comm>
2022-09-01 00:43:43 -0400

    first commit

---
+++ src/main/java/PrisonTestStudent.java
@@ -0,0 +1,12 @@
+//Section 7, Lesson 3 Starter for Exercise 1 - Slide 6
+
+public class PrisonTestStudent {
+
+    public static void main(String[] args) {
+        PrisonerStudent bubba = new PrisonerStudent("Bubba", 6.10, 4);
+        PrisonerStudent twitch = new PrisonerStudent("Twitch", 5.10, 3);
+
+
+        System.out.println(bubba.name);
+    }
+}
---
+++ src/main/java/PrisonerStudent.java
@@ -0,0 +1,22 @@
+//Section 7, Lesson 3 Starter for Exercise 1 - Slide 6
+
+public class PrisonerStudent {
+
+    //Fields
+    public String name;
+    public double height;
+    public int sentence;
+
+    public PrisonerStudent(String name, double height, int sentence){
+        name = this.name;
+        height = this.height;
+        sentence = this.sentence;
+    }
+
+    //Methods
+    public void think(){
+        System.out.println("I'll have my revenge.");
+    }
+
+}
+
